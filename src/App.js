d98ab3f7c172b6b5e1dcc4431e5379acd777973f
ouyazici <onurulas_57@hotmail.com>
2023-04-28 17:44:55 +0300

    Gorev Tamamlandı

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
 
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
-import MovieHeader from './components/MovieHeader';
+import MovieHeader from "./components/MovieHeader";
 
-import AddMovieForm from './components/AddMovieForm';
-import FavoriteMovieList from './components/FavoriteMovieList';
+import AddMovieForm from "./components/AddMovieForm";
+import FavoriteMovieList from "./components/FavoriteMovieList";
+import { useSelector } from "react-redux";
 
-const App = props => {
-  const displayFavorites = true;
-
+const App = (props) => {
+  const displayFavorites = useSelector(
+    (store) => store.favoritesReducer.displayFavorites
+  );
   return (
     <div>
       <nav className="bg-zinc-800 px-6 py-3">
--- src/actions/favoritesActions.js
+++ src/actions/favoritesActions.js
@@ -3,13 +3,16 @@ export const ADD_FAVORITE = "ADD_FAVORITE";
 export const REMOVE_FAVORITE = "REMOVE_FAVORITE";
 
 export const toggleFavorites = () => {
-  return ({ type: TOGGLE_FAVORITES });
-}
+  return { type: TOGGLE_FAVORITES };
+};
 
 export const addFavorite = (movie) => {
-  return ({ type: ADD_FAVORITE, payload: movie });
-}
+  const favMovie = { id: movie.id, title: movie.title };
+  return { type: ADD_FAVORITE, payload: favMovie };
+};
 
 export const removeFavorite = (id) => {
-  return ({ type: REMOVE_FAVORITE, payload: id });
-}
+  return { type: REMOVE_FAVORITE, payload: id };
+};
+
+// Path: src/components/Favorites.js
